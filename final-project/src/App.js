ee0100954bb9452a568eb98eaeae0a80d8cb7eeb
Davidmitseruk <davidmitseruk@gmail.com>
2026-03-30 15:15:22 +0200

    ok

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,6 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import UserPage from "./pages/UserPage";
+import CreateUserPage from "./pages/CreateUserPage";
 
 function App() {
-  return 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/users" element={<UserPage />} />
+        <Route path="/create" element={<CreateUserPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
